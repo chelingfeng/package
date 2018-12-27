2d22ff570c6fd620136106e69c22c1594cd6eb0e
chelingfeng <chelingfeng@edusoho.com>
2018-12-27 17:36:19 +0800

    init

---
+++ src/Service/UserService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Chelingfeng\Package\Service;
+
+class UserService
+{
+    public function getUser()
+    {
+        return [
+            'id' => 1,
+            'name' => 'chelingfeng',
+        ];
+    }
+}
